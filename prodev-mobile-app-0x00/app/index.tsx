180e0a421a71f83c2e89471568f3a899aa5bb8d6
Grace Ghansah <graceghansah2000@gmail.com>
2026-01-11 23:27:42 +0000

    update

---
+++ prodev-mobile-app-0x00/app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
+        **First App Created**
+      </Text>
+    </View>
+  );
+}
